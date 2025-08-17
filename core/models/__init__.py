a467c7dae91b8f43d78d334172b51fa72186bf40
pc home <you@example.com>
2025-08-18 01:20:04 +0330

    add init

---
+++ core/models/__init__.py
@@ -0,0 +1,12 @@
+from .branch import Branch
+from .currency import Currency
+from .customer import Customer
+from .picture import Picture
+from .price import Price
+from .staff_branch import StaffBranch
+from .staff import Staff
+from .transaction import Transaction
+
+
+__all__=['Branch','Currency','Customer','Picture','Price'
+         ,'StaffBranch','Staff','Transaction']
